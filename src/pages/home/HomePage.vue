ac781f88006d2f52512b19a390daf57d537d8ca3
Lucas Rodríguez <lucasrdz994@gmail.com>
2024-06-03 20:21:31 -0300

    home page styles improve

--- src/pages/home/HomePage.vue
+++ src/pages/home/HomePage.vue
@@ -5,9 +5,9 @@ import pikachu from '@/assets/pikachu.svg'
 
 <template>
   <section class="grid place-items-center h-dvh">
-    <div class="w-1/2 max-h-full flex flex-col items-center">
+    <div class="px-7 md:px-0 md:w-1/2 max-h-full flex flex-col items-center">
       <img class="mb-[60px]" :src="pikachu" alt="pikachu greeting" />
-      <h1 class="font-bold text-[1.444rem] mb-[30px]">Welcome to Pokedex</h1>
+      <h1 class="font-bold text-center text-[1.444rem] mb-[30px]">Welcome to Pokedex</h1>
       <p class="mb-[50px] text-center">
         The digital encyclopedia created by Professor Oak is an invaluable tool to Trainers in the Pokémon world.
       </p>
